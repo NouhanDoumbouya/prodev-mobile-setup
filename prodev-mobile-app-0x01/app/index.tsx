a181a28d5f69f9a93566d92511fc4a59c8ecb88c
NouhanDoumbouya <nouhandoumbouya655@gmail.com>
2025-11-06 23:40:36 +0800

    resolved

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,55 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+<ParallaxScrollView
+  headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+  headerImage={<ThemedView />}  // No image displayed
+>
+      <ThemedView style={styles.container}>
+        <ThemedText type="title">Entry Screen - Awesome</ThemedText>
+        <HelloWave />
+      </ThemedView>
+
+      <ThemedView>
+        <ThemedText style={styles.largeText}>Typescript is great if you practice more</ThemedText>
+        <ThemedText style={styles.mediumText}>React Native provides you a single codebase for cross platforms</ThemedText>
+        <ThemedText style={styles.smallText}>ALX is awesome</ThemedText>
+      </ThemedView>
+
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#90caf9",
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+});
